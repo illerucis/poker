7631e938b528d57819c06c51c5dcc364dc0a1f66
illerucis <rob.sicurelli@gmail.com>
2013-10-01 13:34:02 -0400

    grouping

--- handeval.jl
+++ handeval.jl
@@ -13,12 +13,50 @@
 #     group cards by numbers, separately test for flush
 
 
+#     ((4, 1), (6, 2))
+
+#     (4, 1) => 8
+
 function unpackhand(hand)
-    [ index("-23456789TJQKA", rank) for (rank, suit) in hand ]
+    [ search("-23456789TJQKA", rank) for (rank, suit) in hand ]
 end
-    
+
+function countranks(ranks)
+    counts = Dict{Integer, Integer}()
+    for rank in ranks
+        if haskey(counts, rank)
+            counts[rank] += 1
+        else
+            counts[rank] = 1
+        end
+    end
+    return counts
+end
+
+function groupranks(ranks)
+
+    # dictionary of rank to counts
+    rankcounts = countranks(ranks)
+
+    groupedranks = Array(Tuple, length(keys(rankcounts)))
+    i = 1
+    for rank in keys(rankcounts)
+        groupedranks[i] = (rankcounts[rank], rank)
+        i += 1
+    end
+    groupedranks = sort(groupedranks, by=getfirstelem, rev=true)
+    println(groupedranks)
+end
+
+function getfirstelem(iter)
+    return iter[1]
+end
 
 function evalhand(hand)
-    # unpack hands
+    # Unpack hands
     ranks = unpackhand(hand)
+    grouped = groupranks(ranks)
 end
+
+hand = [('2', 'D'), ('3', 'S'), ('K', 'D'), ('K', 'H'), ('6', 'C')]
+evalhand(hand)
