af416d1db8eb709212733fd4f916b012557fd6cd
illerucis <rob.sicurelli@gmail.com>
2013-10-03 14:36:39 -0400

    more tests

--- src/pokerhand.jl
+++ src/pokerhand.jl
@@ -1,6 +1,6 @@
 # hands something like [('2', 'D'), ('3', 'S'), ('4', 'D'), ('5', 'H'), ('6', 'C')]
 #
-# straight flush - 9
+# straight flush - 11
 # four of a kind - 8
 # full house - 7
 # flush - 6
@@ -17,7 +17,21 @@
 # (4, 1) implies four of a kind 
 
 type Hand
+
+    cards::ASCIIString
     hand::Array
+
+    function Hand(cards::ASCIIString)
+
+        hand = Array(Tuple, 5)
+        i = 1
+        for card in split(cards, " ")
+            hand[i] = (card[1], card[2])
+            i += 1
+        end
+
+        new(cards, hand)
+    end
 end
 
 function Base.isless(myhand::Hand, otherhand::Hand)
@@ -25,7 +39,7 @@ function Base.isless(myhand::Hand, otherhand::Hand)
     myresult = evaluate(myhand)
     otherresult = evaluate(otherhand)
 
-    return ( myresult[1] < otherresult[1] )  || ( myresult[1] == otherresult[1] && myresult[2] <= otherresult[2] )
+    return ( myresult[1] < otherresult[1] )  || ( myresult[1] == otherresult[1] && myresult[2] < otherresult[2] )
 
 end
 
@@ -39,9 +53,10 @@ end
 
 function countranks(ranks)
 
-    counts = Dict()
+    counts = Dict{Integer, Integer}()
 
     for rank in ranks
+
         if haskey(counts, rank)
             counts[rank] += 1
         else
@@ -64,13 +79,14 @@ function groupranks(ranks)
         groupedranks[i] = (rankcounts[rank], rank)
         i += 1
     end
-    
+
     return zip(sort(groupedranks, rev=true)...)
+
 end
 
 function evaluate(h::Hand)
     
-    handspoints = {(4,) => 8,
+    handspoints = {(4, 1) => 8,
                    (3, 2) => 7,
                    (3, 1, 1) => 4,
                    (2, 2, 1) => 3,
@@ -79,10 +95,10 @@ function evaluate(h::Hand)
              
     # Unpack hand
     ranks = [search("-23456789TJQKA", rank) for (rank, suit) in h.hand]
-
+    
     # group counts and ranks
     counts, ranks = groupranks(ranks)
-    
+
     # check for two hand types that the count hash won't give us
     isstraight = ( length(counts) == 5 ) && ( ranks[1] - ranks[5] == 4 )
     isflush = length(keys( {  s => s for (r, s) in h.hand } )) == 1
--- test/test.jl
+++ test/test.jl
@@ -1,25 +1,11 @@
 using Poker
 using FactCheck
 
-function createtuplehand(line)
-
-    hand = Array(Tuple, 5)
-    
-    i = 1
-    for card in split(line, " ")
-        hand[i] = (card[1], card[2])
-        i += 1
-    end
-
-    return hand
-
-end
-
 function parseintohands(line)
 
     midway = int(length(line)/2)
     
-    return Hand(createtuplehand(strip(line[1:midway]))), Hand(createtuplehand(strip(line[midway:])))
+    return Hand(strip(line[1:midway])), Hand(strip(line[midway:]))
     
 end
 
@@ -48,29 +34,104 @@ end
 
 facts("Test suite for Poker's hand evaluator") do
     
-    context("Full evaluator run solves Project Euler #54") do
+    context("Test that solves Project Euler #54") do
         
         winners, losers = testeuler54()
-
+        
         @fact winners => 376
         @fact losers => 624
 
     end
 
-    context("The Hand type's isless and isequal functions") do
+    context("Test evaluate()") do
 
-        # full house, 3 2's, 2 3's
-        fullhouse1 = Hand([("2", "D"), ("3", "S"), ("2", "C"), ("2", "H"), ("3", "D")])
+        # high card ace kicker
+        highcardAce = Hand("2D 3S 4H 5C AD")
+        points, ranks = evaluate(highcardAce)
+        @fact points => 1
+        @fact ranks => (14, 5, 4, 3, 2)
 
-        # full house, 3 3's, 2 2's
-        fullhouse2 = Hand([("3", "D"), ("2", "S"), ("3", "C"), ("3", "H"), ("2", "D")])
+        # one pair
+        onepair = Hand("2D 2H 3H 4D AS")
+        points, ranks = evaluate(onepair)
+        @fact points => 2
+        @fact ranks => (2, 14, 4, 3)
 
-        @fact true => fullhouse1 < fullhouse2
-        @fact false => fullhouse2 <= fullhouse1
-        @fact false => fullhouse1 == fullhouse2
-        @fact false => fullhouse1 > fullhouse2
-        @fact false => fullhouse1 >= fullhouse2
+        # two pair
+        twopair = Hand("5D 7C 5S 8D 7H")
+        points, ranks = evaluate(twopair)
+        @fact points => 3
+        @fact ranks => (7, 5, 8)
 
+        # three of a kind
+        threeofakind = Hand("AD AH AS KD 4S")
+        points, ranks = evaluate(threeofakind)
+        @fact points => 4
+        @fact ranks => (14, 13, 4)
+
+        # straight
+        straight = Hand("2D 3H 4S 5D 6H")
+        points, ranks = evaluate(straight)
+        @fact points => 5
+        @fact ranks => (6, 5, 4, 3, 2)
+
+        # flush
+        flush = Hand("2D 5D 8D KD QD")
+        points, ranks = evaluate(flush)
+        @fact points => 6
+        @fact ranks => (13, 12, 8, 5, 2)
+
+        # full house
+        fullhouse = Hand("5D KH 5S KD KC")
+        points, ranks = evaluate(fullhouse)
+        @fact points => 7
+        @fact ranks => (13, 5)
+
+        # four of a kind
+        fourofakind = Hand("3D 3H 3S 3C AH")
+        points, ranks = evaluate(fourofakind)
+        @fact points => 8
+        @fact ranks => (3, 14)
+
+        # straight flush
+        straightflush = Hand("8H 9H TH JH QH")
+        points, ranks = evaluate(straightflush)
+        @fact points => 11
+        @fact ranks => (12, 11, 10, 9, 8)
+        
     end
 
+    context("Test isless and isequal") do
+
+        # full house, 3 2's, 2 3's
+        fullhouse1 = Hand("2D 3S 2C 2H 3D")
+
+        # full house, 3 3's, 2 2's
+        fullhouse2 = Hand("3D 2S 3C 3H 2D")
+        
+        @fact true => fullhouse1 < fullhouse2
+        @fact true => fullhouse2 > fullhouse1
+        @fact true => fullhouse2 != fullhouse1
+
+        # high card ace kicker
+        highcardAce = Hand("2D 3S 4H 5C AD")
+        # high card king kicker
+        highcardKing = Hand("2D 3S 4H 5C KD")
+        
+        @fact true => highcardKing < highcardAce
+        @fact true => highcardAce > highcardKing
+        @fact true => highcardAce != highcardKing
+
+        # high card ace kicker
+        highcardAce1 = Hand("2D 3S 4H 5C AD")
+        highcardAce2 = Hand("AD 3S 5C 4H 2D")
+
+        @fact true => highcardAce1 == highcardAce2
+        
+    end
+
+    context("countrank(ranks) tests") do
+    end
+    
+    
 end
